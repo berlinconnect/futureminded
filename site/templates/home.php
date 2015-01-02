cba47dd1738421b773666cb9cd714f058cd935c3
Timothy Achumba <tim@6wunderkinder.com>
2015-01-02 22:06:09 +0100

    Added the row of services to the page

--- site/templates/home.php
+++ site/templates/home.php
@@ -5,10 +5,34 @@
 <section class="intro center">
   <h3><?= $page->intro() ?></h3>
 </section>
+
 <div class="divider"></div>
+
 <section class="about center">
   <h2><?= $page->aboutheadline() ?></h2>
   <?= $page->about()->kirbytext() ?>
-<section>
+</section>
+
+<section class="services center">
+  <div class="wrapper">
+    <div class="cols">
+      <div class="col col-33">
+        <img src="img/education.svg" alt="education">
+        <h4><?= $page->educationheadline() ?></h4>
+        <p class="small"><?= $page->education() ?></p>
+      </div>
+      <div class="col col-33">
+        <img src="img/engagement.svg" alt="education">
+        <h4><?= $page->engagementheadline() ?></h4>
+        <p class="small"><?= $page->engagement() ?></p>
+      </div>
+      <div class="col col-33">
+        <img src="img/business.svg" alt="education">
+        <h4><?= $page->businessheadline() ?></h4>
+        <p class="small"><?= $page->business() ?></p>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php snippet('footer') ?>
