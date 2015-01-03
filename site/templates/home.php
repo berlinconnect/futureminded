f41e03c6d396ffa55465134a2c0bc9771c1e1b3f
Timothy Achumba <tim@6wunderkinder.com>
2015-01-03 12:46:43 +0100

    Styled the projects

--- site/templates/home.php
+++ site/templates/home.php
@@ -36,30 +36,39 @@
 </section>
 
 <section class="projects">
-  <div class="description">
-    <h2><?= $page->projectheadline() ?></h2>
-    <?= $page->project()->kirbytext() ?>
-    <ul class="projet-navigation">
-      <?php foreach($pages->find('projects')->children()->visible()->flip() as $project): ?>
-      <li>
-        <a class="logo" style="background-image: url(<?php echo $project->images()->last()->url(); ?>)">
-        </a>
-      </li>
-      <?php endforeach ?>
-    </ul>
-  </div>
-  <div class="project-container">
-    <?php foreach($pages->find('projects')->children() as $project): ?>
-      <div id="<?= $project->indentifier() ?>" class="project active" style="background-image: url(<?php echo $project->images()->first()->url(); ?>)">
-        <div class="overlay"></div>
-        <div class="content">
-          <h3 class="big"><?= $project->intro() ?></h3>
-          <?= $project->text()->kirbytext() ?>
-          <a class="button"><?= $page->more() ?></a>
-        </div>
+  <div class="project-wrapper">
+    <div class="container">
+      <div class="description">
+        <h2><?= $page->projectheadline() ?></h2>
+        <?= $page->project()->kirbytext() ?>
+        <ul class="projet-navigation">
+          <?php foreach($pages->find('projects')->children()->visible()->flip() as $project): ?>
+          <li>
+            <a class="logo" style="background-image: url(<?php echo $project->images()->last()->url(); ?>)">
+            </a>
+          </li>
+          <?php endforeach ?>
+        </ul>
       </div>
-    <?php endforeach ?>
+      <div class="project-container">
+        <?php foreach($pages->find('projects')->children() as $project): ?>
+          <div class="project active <?= $project->identifier() ?>">
+            <div class="content">
+              <h3 class="big"><?= $project->intro() ?></h3>
+              <?= $project->text()->kirbytext() ?>
+              <a class="button"><?= $page->more() ?></a>
+            </div>
+          </div>
+        <?php endforeach ?>
+      </div>
+    </div>
   </div>
+  <div class="overlay"></div>
+  <?php foreach($pages->find('projects')->children() as $project): ?>
+    <div class="project-background active <?= $project->identifier() ?>" style="background-image: url(<?php echo $project->images()->first()->url(); ?>)">
+    </div>
+  <?php endforeach ?>
+
 </section>
 
 <?php snippet('footer') ?>
