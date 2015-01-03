c2258aa01975e577fca2e7fc672af44aec8634f5
Timothy Achumba <tim@6wunderkinder.com>
2015-01-03 12:04:05 +0100

    Set up projects

--- site/templates/home.php
+++ site/templates/home.php
@@ -13,7 +13,7 @@
   <?= $page->about()->kirbytext() ?>
 </section>
 
-<section class="services center">
+<section class="services hidden center">
   <div class="wrapper">
     <div class="cols">
       <div class="col col-33">
@@ -32,6 +32,33 @@
         <p class="small"><?= $page->business() ?></p>
       </div>
     </div>
+  </div>
+</section>
+
+<section class="projects">
+  <div class="description">
+    <h2><?= $page->projectheadline() ?></h2>
+    <?= $page->project()->kirbytext() ?>
+    <ul class="projet-navigation">
+      <?php foreach($pages->find('projects')->children()->visible()->flip() as $project): ?>
+      <li>
+        <a class="logo" style="background-image: url(<?php echo $project->images()->last()->url(); ?>)">
+        </a>
+      </li>
+      <?php endforeach ?>
+    </ul>
+  </div>
+  <div class="project-container">
+    <?php foreach($pages->find('projects')->children() as $project): ?>
+      <div id="<?= $project->indentifier() ?>" class="project active" style="background-image: url(<?php echo $project->images()->first()->url(); ?>)">
+        <div class="overlay"></div>
+        <div class="content">
+          <h3 class="big"><?= $project->intro() ?></h3>
+          <?= $project->text()->kirbytext() ?>
+          <a class="button"><?= $page->more() ?></a>
+        </div>
+      </div>
+    <?php endforeach ?>
   </div>
 </section>
 
