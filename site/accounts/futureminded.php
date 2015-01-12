0685afbe2283d36f8759441ac4dd9a7e889f378b
Timothy Achumba <tim@6wunderkinder.com>
2015-01-12 23:20:15 +0100

    New user

---
+++ site/accounts/futureminded.php
@@ -0,0 +1,17 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: futureminded
+firstname:
+lastname:
+email: linda@futureminded.de
+password: >
+  $2a$10$sqAplAKWsNM9.WyHo/BmL.bik0s/NBku7quiDOAEuFohUu1/WGLX.
+language: en
+role: admin
+history:
+  - home
+  - null
+  - imprint
+  - error
+  - projects/shine-strength
+token: fd6a9660b3e97b7485256905122fa75a9613a54d
