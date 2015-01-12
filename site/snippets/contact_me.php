b66e5bdd718d8b3fe2c679947a0991a4a102d0fc
Timothy Achumba <tim@6wunderkinder.com>
2015-01-12 22:20:41 +0100

    Moved the js to its own file

--- site/snippets/contact_me.php
+++ site/snippets/contact_me.php
@@ -1,24 +1,24 @@
 <?php
 if($_POST)
 {
-    $to_email       = "hausberger.pascal@gmail.com"; //Recipient email, Replace with own email here
-    
+    $to_email = "timothy@achumba.com"; //Recipient email, Replace with own email here
+
     //check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-        
+
         $output = json_encode(array( //create JSON data
-            'type'=>'error', 
+            'type'=>'error',
             'text' => 'Sorry Request must be Ajax POST'
         ));
         die($output); //exit script outputting json data
-    } 
-    
+    }
+
     //Sanitize input data using PHP filter_var().
     $user_name      = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
     $user_email     = filter_var($_POST["user_email"], FILTER_SANITIZE_EMAIL);
     $user_org       = filter_var($_POST["user_org"], FILTER_SANITIZE_STRING);
     $message        = filter_var($_POST["msg"], FILTER_SANITIZE_STRING);
-    
+
     //additional php validation
     if(strlen($user_name)<3){ // If length is less than 3 it will output JSON error.
         $output = json_encode(array('type'=>'error', 'text' => 'Name is too short!'));
@@ -36,17 +36,17 @@ if($_POST)
         $output = json_encode(array('type'=>'error', 'text' => 'Too short message! Please enter something.'));
         die($output);
     }
-    
+
     //email body
     $message_body = $message."\r\n\r\n-".$user_name."\r\nEmail : ".$user_email."\r\nOrganisation  : (".$user_org.") " ;
-    
+
     //proceed with PHP email.
     $headers = 'From: '.$user_name.'' . "\r\n" .
     'Reply-To: '.$user_email.'' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
-    
+
     $send_mail = mail($to_email, $subject, $message_body, $headers);
-    
+
     if(!$send_mail)
     {
         //If mail couldn't be sent output error. Check your PHP email configuration (if it ever happens)
