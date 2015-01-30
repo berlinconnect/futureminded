c079ad5eb496034d0665a73e597d804a0c97abdc
Timothy Achumba <tim@6wunderkinder.com>
2015-01-30 14:48:56 +0100

    Fixed the twitter icon size

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -7,7 +7,6 @@
     <ul class="menu">
       <li><a href="/imprint"><?= $site->imprint() ?></a></li>
       <li><a href="/press"><?= $site->press() ?></a></li>
-      <li><a>Twitter</a></li>
       <li><a href="https://twitter.com/future_minded" target="_blank"><img src="../img/twitter_logo.svg" class="twitterLogo"></a></li>
     </ul>
   </div>
