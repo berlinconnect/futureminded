fb995fc28c8b1b4137384c3e70c3e59d0ac73933
Timothy Achumba <tim@6wunderkinder.com>
2015-01-30 10:39:12 +0100

    Updated users

--- site/accounts/futureminded.php
+++ site/accounts/futureminded.php
@@ -5,13 +5,6 @@ firstname:
 lastname:
 email: linda@futureminded.de
 password: >
-  $2a$10$sqAplAKWsNM9.WyHo/BmL.bik0s/NBku7quiDOAEuFohUu1/WGLX.
+  $2a$10$vJoiwF4gF.dh9ad0dnKwpeeROkDG.eveHyeRK0IEZ94d7IprRfWgu
 language: en
 role: admin
-history:
-  - home
-  - null
-  - imprint
-  - error
-  - projects/shine-strength
-token: fd6a9660b3e97b7485256905122fa75a9613a54d
--- site/accounts/timothy.php
+++ site/accounts/timothy.php
@@ -3,14 +3,7 @@
 username: timothy
 email: timothy@achumba.com
 password: >
-  $2a$10$/A6V6I3qLgtIhV9QUgMhqOxMenQCS2/tXvKByQEHEZ5h1Our0SHcG
+  $2a$10$OmdtzbnGTD5yuN75WjBspeSzS/cXZnhtZCE2.tSlUMCQB3172uJ7i
 language: en
 role: admin
-token: a3fb557ab4ab7cf49816d64ac8ee1b4ccbc87ecd
-history:
-  - >
-    press/keeping-berlins-next-generation-of-innovators
-  - projects/shine-strength
-  - projects/walk-for-freedom
-  - projects/charity-water
-  - projects/city-kindness
+token: 6f6f76cda5532779cf5c545718d8f7e6c7812df1
