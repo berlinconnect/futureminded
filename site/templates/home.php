f340be036c32e0a0b309e15d6efe04f4d0afd176
Timothy Achumba <tim@6wunderkinder.com>
2015-01-30 14:45:08 +0100

    Complied css and removed the link to later add on the server

--- site/templates/home.php
+++ site/templates/home.php
@@ -99,7 +99,7 @@
   <div class="content cols">
     <div class="speaker col col-60">
       <div class="copy">
-        <a href='http://2020plus.net/berlin/'><h2><?= $page->speakerheadline() ?></h2></a>
+        <h2><?= $page->speakerheadline() ?></h2>
         <?= $page->speaker()->kirbytext() ?>
       </div>
     </div>
