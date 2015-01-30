23a2fdec232b0a1a38f006724b4065c7a74824dd
hauspa <pasci@pascals-MacBook-Pro.local>
2015-01-30 10:10:30 +0100

    Added Twitter link and logo, but position not done
    I wasn’t able to position the twitter icon correctly, could u plz tell
    me how to? thx!
    text works (i left “twitter” in there for now), but when i insert the
    same tags with an image, it doesnt seem to work the same way…
    thx!

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -7,6 +7,8 @@
     <ul class="menu">
       <li><a href="/imprint"><?= $site->imprint() ?></a></li>
       <li><a href="/press"><?= $site->press() ?></a></li>
+      <li><a>Twitter</a></li>
+      <li><a href="https://twitter.com/future_minded" target="_blank"><img src="../img/twitter_logo.svg" class="twitterLogo"></a></li>
     </ul>
   </div>
 </footer>
